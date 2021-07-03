40c7024adad678e5d503aa705f14241761b2247b
CsAlkemy <alkemy48@gmail.com>
2021-07-03 12:55:26 +0600

    init

---
+++ src/components/Home/sideNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link to="/" class="p-3">
+    <VueHero />
+  </router-link>
+</template>
+
+<script>
+import VueHero from "@/components/About/vueHero";
+export default {
+  name: "sideNav",
+  components: { VueHero },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
